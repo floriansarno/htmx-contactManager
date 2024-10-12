df7250ffb8abd9e3825556388a4b203f51e4ed09
Florian Sarno <florian.sarno@husqvarnagroup.com>
2024-10-13 00:48:38 +0800

    Initial changes

---
+++ routes/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const contacts = [
+  { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
+  { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
+  { id: 3, name: 'Emily Johnson', email: 'emily.johnson@example.com' },
+  { id: 4, name: 'Aarav Patel', email: 'aarav.patel@example.com' },
+  { id: 5, name: 'Liu Wei', email: 'liu.wei@example.com' },
+  { id: 6, name: 'Fatima Zahra', email: 'fatima.zahra@example.com' },
+  { id: 7, name: 'Carlos Hernández', email: 'carlos.hernandez@example.com' },
+  { id: 8, name: 'Olivia Kim', email: 'olivia.kim@example.com' },
+  { id: 9, name: 'Kwame Nkrumah', email: 'kwame.nkrumah@example.com' },
+  { id: 10, name: 'Chen Yu', email: 'chen.yu@example.com' },
+];
+
+// GET /contacts
+router.get('/contacts', (req, res) => {
+  res.render('index', { contacts });
+});
+
+// GET /contacts/new
+router.get('/contacts/new', (req, res) => {
+  if (req.headers['hx-request']) {
+    res.render('form');
+  } else {
+    res.render('index', { action: 'new', contacts, contact: {}});
+  }
+});
+
+// GET /contacts/1
+router.get('/contacts/:id', (req, res) => {
+  const { id } = req.params;
+  const contact = contacts.find((c) => c.id === Number(id));
+
+  if (req.headers['hx-request']) {
+    res.render('contact', { contact });
+  } else {
+    res.render('index', { action: 'show', contacts, contact});
+  }
+});
+
+// POST /contacts
+router.post('/contacts', (req, res) => {
+  const newContact = {
+    id: contacts.length + 1,
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  contacts.push(newContact);
+
+  if (req.headers['hx-request']) {
+    res.render('sidebar', { contacts });
+  } else {
+    res.render('index', { action: 'new', contacts, contact: {} });
+  }
+
+});
+
+module.exports = router;
